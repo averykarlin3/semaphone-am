8560d420bee84d27b20856dc481ab1aa3142a6c0
matthew0367 <matthew0367@gmail.com>
2015-12-13 15:43:00 -0500

    Fixed errors Invalid Argument and File errors in -r

--- control.c
+++ control.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char *argv[]) {
 	int f, sh, sem;
-	int shkey = ftok("story", 0);
-	int smkey = ftok("story", 1);
+	int shkey = ftok(".averybadatdebugging", 3);
+	int smkey = ftok(".averybadatdebugging", 4);
 	if(!strcmp(argv[1], "-r")) {
 		sh = shmget(shkey, sizeof(int), 0644);
-		sem = semget(smkey, sizeof(int), 0644);
+		sem = semget(smkey, 1, 0644);
 		int check = shmctl(sh, IPC_RMID, NULL);
 		if(check == -1) {
 			printf("Error: %s\n", strerror(errno));
@@ -15,15 +15,14 @@ int main(int argc, char *argv[]) {
 		if(check == -1) {
 			printf("Error: %s\n", strerror(errno));
 		}
-		printf("hi\n");
 		f = open("story", O_RDONLY, 0444);
-		if(check == -1) {
+		if(f == -1) {
 			printf("Error: %s\n", strerror(errno));
 		}
 		struct stat sfile;
 		stat("story", &sfile);
 		char fst[sfile.st_size];
-		check = read(check, fst, sfile.st_size);
+		check = read(f, fst, sfile.st_size);
 		if(check == -1) {
 			printf("Error: %s\n", strerror(errno));
 		}
@@ -31,16 +30,16 @@ int main(int argc, char *argv[]) {
 		close(f);
 	}
 	else if(!strcmp(argv[1], "-c")) {
-		f = open("story", O_CREAT | O_RDWR, 0666);
-		if(f < 0) {
-			printf("Error: %s\n", strerror(errno));
-		}
-		sh = shmget(shkey, sizeof(int), 0666 | IPC_CREAT);
+		sh = shmget(shkey, sizeof(int), 0644 | IPC_CREAT);
 		if(sh < 0) {
 			printf("Error: %s\n", strerror(errno));
 		}
-		sem = semget(smkey, 1, IPC_CREAT);
+		sem = semget(smkey, 1, 0644 | IPC_CREAT);
 		if(sem < 0) {
+			printf("Error: %s\n", strerror(errno));
+		}
+		f = open("story", O_CREAT | O_RDWR, 0644);
+		if(f < 0) {
 			printf("Error: %s\n", strerror(errno));
 		}
 		close(f);
